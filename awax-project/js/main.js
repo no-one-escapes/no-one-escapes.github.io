4fabe39f7f87d2c6178d24ea572460fd39f3cae5
no-one-escapes <54147982+no-one-escapes@users.noreply.github.com>
2019-08-31 17:04:46 +0300

    Add files via upload

--- awax-project/js/main.js
+++ awax-project/js/main.js
@@ -3,27 +3,42 @@ $(document).ready(function () {
         dots: true,
         arrows: false
     });
-});
 
-$(document).ready(function () {
     $('.slider__clients-card').slick({
         dots: true,
         arrows: false
     });
-});
 
-$(document).ready(function () {
     $('.section-five__team').slick({
         dots: true,
         arrows: false,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1150,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
-});
 
-$(document).ready(function () {
     $('.section-nine__slider').slick({
         dots: true,
         arrows: false
     });
+
 });
+
+
+
+
