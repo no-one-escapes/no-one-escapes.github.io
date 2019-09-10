ce530f43aca08e9443abb9c5c78d0f7eda91693f
no-one-escapes <54147982+no-one-escapes@users.noreply.github.com>
2019-09-10 22:42:01 +0300

    Update main.js

--- game-sea-battle/js/main.js
+++ game-sea-battle/js/main.js
@@ -10,11 +10,11 @@ let rightPressed = false;
 let leftPressed = false;
 let topPressed = false;
 let botPressed = false;
-let wallX = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480, 510, 540, 
+let shipX = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480, 510, 540, 
     570, 600, 630, 660, 690, 720, 750, 780, 810, 840, 870, 900, 930, 960, 990, 1020, 1050, 1080];
-let wallY = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450];
-let wallWidth = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480, 510, 540, 570, 600];
-let wallHeight = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480, 510, 540, 570, 600];
+let shipY = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450];
+let shipWidth = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480, 510, 540, 570, 600];
+let shipHeight = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480, 510, 540, 570, 600];
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -54,7 +54,7 @@ function drawRect() {
 
 function drawOne(){
     ctx.beginPath(); // 1
-    ctx.rect(wallX[4], wallY[10], wallWidth[1], wallHeight[1]);
+    ctx.rect(shipX[4], shipY[10], shipWidth[1], shipHeight[1]);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.strokeStyle = "black";
@@ -65,7 +65,7 @@ function drawOne(){
 function drawObjects(){
    
     ctx.beginPath(); // 4
-    ctx.rect(wallX[4], wallY[4], wallWidth[4], wallHeight[1]);
+    ctx.rect(shipX[4], shipY[4], shipWidth[4], shipHeight[1]);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.strokeStyle = "black";
@@ -73,7 +73,7 @@ function drawObjects(){
     ctx.stroke();
 
     ctx.beginPath(); // 3
-    ctx.rect(wallX[4], wallY[6], wallWidth[3], wallHeight[1]);
+    ctx.rect(shipX[4], shipY[6], shipWidth[3], shipHeight[1]);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.strokeStyle = "black";
@@ -81,7 +81,7 @@ function drawObjects(){
     ctx.stroke();
 
     ctx.beginPath(); // 2
-    ctx.rect(wallX[4], wallY[8], wallWidth[2], wallHeight[1]);
+    ctx.rect(shipX[4], shipY[8], shipWidth[2], shipHeight[1]);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.strokeStyle = "black";
@@ -89,14 +89,14 @@ function drawObjects(){
     ctx.stroke();
 
     ctx.beginPath(); // first field
-    ctx.rect(wallX[12], wallY[3], wallWidth[10], wallHeight[10]);
+    ctx.rect(shipX[12], shipY[3], shipWidth[10], shipHeight[10]);
     ctx.fillStyle = "transparent";
     ctx.fill();
     ctx.closePath();
     ctx.stroke();
 
     ctx.beginPath(); // second field
-    ctx.rect(wallX[27], wallY[3], wallWidth[10], wallHeight[10]);
+    ctx.rect(shipX[27], shipY[3], shipWidth[10], shipHeight[10]);
     ctx.fillStyle = "transparent";
     ctx.fill();
     ctx.strokeStyle = "black";
@@ -147,13 +147,13 @@ function drawContur(){
 
 function drawBackground(){
     ctx.beginPath(); // first field
-    ctx.rect(wallX[12], wallY[3], wallWidth[10], wallHeight[10]);
+    ctx.rect(shipX[12], shipY[3], shipWidth[10], shipHeight[10]);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.closePath();
 
     ctx.beginPath(); // second field
-    ctx.rect(wallX[27], wallY[3], wallWidth[10], wallHeight[10]);
+    ctx.rect(shipX[27], shipY[3], shipWidth[10], shipHeight[10]);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.closePath();
