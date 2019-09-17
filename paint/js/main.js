2f26dd67a3cd92c35660452cc943cc69b97f36bc
no-one-escapes <54147982+no-one-escapes@users.noreply.github.com>
2019-09-17 16:25:49 +0300

    Add files via upload

--- paint/js/main.js
+++ paint/js/main.js
@@ -1,1 +1,26 @@
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
 
+canvas.onmousedown = function(event){
+    canvas.onmousemove = draw;
+    
+    function draw(event){
+    let x = event.offsetX;
+    let y = event.offsetY;
+    
+
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+    // ctx.beginPath();
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.moveTo(x, y);
+    // ctx.closePath();
+
+    // requestAnimationFrame(draw);
+    }
+    canvas.onmouseup = function (event){
+        canvas.onmousemove = null;
+    }
+
+}
