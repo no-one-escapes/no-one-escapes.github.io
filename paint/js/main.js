6a0181ea5eafd72bea1a9b4581c0b2d83c89cb2c
no-one-escapes <54147982+no-one-escapes@users.noreply.github.com>
2019-09-17 21:31:34 +0300

    Add files via upload

--- paint/js/main.js
+++ paint/js/main.js
@@ -1,26 +1,41 @@
 let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
+let userColor = "black";
+let userWidthBrush = 10;
 
-canvas.onmousedown = function(event){
-    canvas.onmousemove = draw;
-    
-    function draw(event){
-    let x = event.offsetX;
-    let y = event.offsetY;
-    
-
-    ctx.lineWidth = 10;
-    ctx.lineCap = "round";
-    // ctx.beginPath();
-    ctx.lineTo(x, y);
-    ctx.stroke();
-    ctx.moveTo(x, y);
-    // ctx.closePath();
-
-    // requestAnimationFrame(draw);
-    }
-    canvas.onmouseup = function (event){
-        canvas.onmousemove = null;
-    }
-
+document.getElementById('color').onchange = function(e){
+    userColor = this.value;
 }
+
+document.getElementById('range').onchange = function(e){
+    userWidthBrush = this.value;
+}
+
+
+let painting = false;
+
+function startDrawing(){
+    painting = true;
+}
+function finishedDrawing(){
+    painting = false;
+    ctx.beginPath();
+}
+
+function draw(e){
+    if(!painting) return;
+    ctx.lineWidth = userWidthBrush;
+    ctx.lineCap = "round";
+
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY);
+    ctx.strokeStyle = userColor;
+    
+    
+}
+
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mouseup", finishedDrawing);
+canvas.addEventListener("mousemove", draw);
