ffa74d019f0beae4e150f012d6dbb2e8d98ce20d
no-one-escapes <54147982+no-one-escapes@users.noreply.github.com>
2019-09-30 16:36:29 +0300

    Update main.js

--- bubbles/js/main.js
+++ bubbles/js/main.js
@@ -1,1 +1,26 @@
 
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d')
+
+
+function drawObj(){
+  a = Math.round(Math.random()*255);
+  b = Math.round(Math.random()*255);
+  c = Math.round(Math.random()*255);
+  colorRGB = 'rgb(' + a + ',' + b + ',' + c + ')' ;
+  x = Math.random() * canvas.width;
+  y = Math.random() * canvas.height;
+  ctx.beginPath();
+  ctx.arc(x,y,15,0,Math.PI * 2,false);
+  ctx.fillStyle = colorRGB;
+  ctx.fill();
+  ctx.stroke();
+}
+
+  for (let i = 0; i < 2000; i++){
+    (function (i) {
+      setTimeout(function () {
+        drawObj();
+        },100 * i + 1);
+            }) (i);
+  }
