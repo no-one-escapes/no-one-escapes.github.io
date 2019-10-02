e836844f300c91b3b09147fdb510b38f54f5cb2e
no-one-escapes <54147982+no-one-escapes@users.noreply.github.com>
2019-10-02 20:32:15 +0300

    Update main.js

--- bubbles/js/main.js
+++ bubbles/js/main.js
@@ -1,7 +1,37 @@
 
+// ----- first canvas -----
+
+let canvasOne = document.getElementById('myCanvasOne');
+let ctxOne = canvasOne.getContext('2d')
+let radius = 40;
+let x = 30;
+let y = 150;
+let dx = 3;
+
+function moveArc() {
+
+    ctxOne.clearRect(0, 0, canvasOne.width, canvasOne.height);
+    ctxOne.beginPath();
+    ctxOne.arc(x, y, radius, 0, Math.PI * 2, false);
+    ctxOne.fillStyle = "blue";
+    ctxOne.fill();
+    ctxOne.closePath();
+
+    if (x > canvasOne.width || x < 0){
+        dx = -dx;
+    }
+
+    x += dx;
+
+    requestAnimationFrame(moveArc);
+}
+
+moveArc();
+
+// ----- second canvas -----
+
 let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d')
-
 
 function drawObj(){
   a = Math.round(Math.random()*255);
