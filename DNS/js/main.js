984e3fcef804c80b683a9324d45e22598f7df681
no-one-escapes <54147982+no-one-escapes@users.noreply.github.com>
2019-12-14 14:37:48 +0300

    Add files via upload

--- DNS/js/main.js
+++ DNS/js/main.js
@@ -1,1 +1,37 @@
+$(document).ready(function () {
+    // $('.masters__info-photo').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     arrows: false,
+    //     fade: true,
+    //     asNavFor: '.masters__slider'
+    //   });
+    //   $('.masters__slider').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     asNavFor: '.masters__info-photo',
+    //     dots: true,
+    //     centerMode: true,
+    //     focusOnSelect: true
+    //   });
+    // $('.masters__info-inner').slick({
+    //     dots: true,
+    //     arrows: false,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    // });
 
+    $('.testimonial__item-slider').slick({
+        infinite: true,
+        speed: 700,
+        slidesToShow: 1,
+    });
+
+    $('.masters__info-inner').slick({
+        infinite: true,
+        dots: true,
+        arrows: false,
+        speed: 700,
+        slidesToShow: 1,
+    });
+});
