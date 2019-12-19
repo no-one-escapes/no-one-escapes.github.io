c9cf9be25fb3c6f5caa561cb2442fd60ccde3197
no-one-escapes <54147982+no-one-escapes@users.noreply.github.com>
2019-12-19 14:15:13 +0300

    Add files via upload

--- DNS/js/main.js
+++ DNS/js/main.js
@@ -25,6 +25,15 @@ $(document).ready(function () {
         infinite: true,
         speed: 700,
         slidesToShow: 1,
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    dots: false,
+                    arrows: false,
+                }
+            }
+        ]
     });
 
     $('.masters__info-inner').slick({
@@ -34,4 +43,74 @@ $(document).ready(function () {
         speed: 700,
         slidesToShow: 1,
     });
+    $('.blog__inner').slick({
+        dots: false,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 2048,
+                settings: "unslick"
+            },
+            {
+                breakpoint: 950,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.gallery__item-photo-line').slick({
+        dots: false,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 2048,
+                settings: "unslick"
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
 });
+
+(function($){
+    $(function() {
+      $('.navbar__menu-burger').on('click', function() {
+        $(this).closest('.navbar')
+          .toggleClass('open');
+          
+      });
+      
+      $('.navbar__list-item').on('click', function() {
+        // do something
+  
+        $(this).closest('.navbar')
+          .removeClass('open');
+      });
+    });
+  })(jQuery);
+
+
+//   (function($){
+//     $(function() {
+//       $('.navbar__menu-burger').on('click', function() {
+//         $(this).closest('.navbar')
+//           .toggleClass('menu_state_open');
+          
+//       });
+      
+//       $('.navbar__list-item').on('click', function() {
+//         // do something
+  
+//         $(this).closest('.navbar')
+//           .removeClass('menu_state_open');
+//       });
+//     });
+//   })(jQuery);
